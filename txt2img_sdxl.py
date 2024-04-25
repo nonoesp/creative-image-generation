56c6177bec1eb398e350f790120b3ca0b99e13f5
Nono Martínez Alonso <mundowarezweb@gmail.com>
2024-04-25 22:00:48 +0200

    Rename SDXL.

---
+++ txt2img_sdxl.py
@@ -0,0 +1,30 @@
+'''
+Diffusion with a stable diffusion pipeline.
+'''
+
+from config import Config
+from utils import clean_text_prompt, ddyymm_hhmmss, save_meta, set_image_path
+from diffusers import StableDiffusionXLPipeline
+import os
+import torch
+
+# Parameters
+Config.STEPS = 1
+Config.PROMPT = 'a scenic landscape at dusk'
+
+# Get image path
+set_image_path()
+
+# Create a Stable Diffusion pipeline
+pipe = StableDiffusionXLPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0")
+pipe.to(Config.TORCH_DEVICE)
+
+# Save metadata
+save_meta(pipe)
+
+# Generate
+generator = torch.Generator(Config.TORCH_DEVICE).manual_seed(Config.SEED)
+image = pipe(Config.PROMPT, num_inference_steps=Config.STEPS, generator=generator).images[0]
+
+# Save the image
+image.save(Config.IMAGE_PATH)
