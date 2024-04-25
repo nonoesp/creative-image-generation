4aebc5d99bac6467d21735efa0a7f5be5ce7c5d5
Nono Martínez Alonso <mundowarezweb@gmail.com>
2024-04-25 19:56:22 +0200

    Update seed parameter example.

--- param_seed.py
+++ param_seed.py
@@ -7,53 +7,40 @@ from utils import clean_prompt, ddyymm_hhmmss
 import torch
 
 # Config
-STEPS = 15
+STEPS = 25
 SEED = 0
-TORCH_DEVICE = 'mps'
-# PROMPT = "a standalone isolated minimalist building in the SEASON in tokyo, japan, raw photo, uhd"
-# VARIANTS = ["fall", "winter", "spring", "summer"]
-PROMPT = "a box of FRUITs, central perspective, oil-painting, uhd"
+TORCH_DEVICE = 'mps' # Set this to 'cuda' if in Colab with GPU, otherwise 'cpu'
+
+# Define a set of prompt and variants.
+PROMPT = "a standalone isolated minimalist building in the SEASON in tokyo, japan, scenic landscape, at dawn, 4k, hd, raw photo, uhd"
 VARIANTS = {
-    'FRUIT': ["banana", "orange", "apple", "pear", "peach"],
+    'SEASON': ["fall", "winter", "spring", "summer"],
 }
 
-# Create a Stable Diffusion pipeline
-print('› Loading pipeline...')
-pipe = StableDiffusionPipeline.from_pretrained('runwayml/stable-diffusion-v1-5')
-pipe.to(TORCH_DEVICE)
-print('› Loaded...')
+# Another example of possible prompt and variants.
+# PROMPT = "a box of FRUITs, central perspective, oil-painting, uhd"
+# VARIANTS = {
+#     'FRUIT': ["banana", "orange", "apple", "pear", "peach"],
+# }
 
-# Prompt variations
+# Create the prompt variations.
 prompts = []
 for key in VARIANTS.keys():
     for variant in VARIANTS[key]:
         prompts.append(PROMPT.replace(key, variant))
 
-print(prompts)
-
-prompts = [
-    "a dark glass of gazpacho",
-    "a wine glass of gazpacho",
-    "a self portrait oil painting of a fireman",
-    "a self portrait oil painting of a fireman, dark",
-    "a self portrait oil painting of a fireman, light",
-    "a japanese house standalone",
-    "a japanese house standalone, concrete",
-    "a japanese house standalone, wood",
-    "a cat",
-    "a cat and a dog",
-    "a motorcycle, red",
-    "a motorcycle, blue",
-]
+# Create a Stable Diffusion pipeline.
+pipe = StableDiffusionPipeline.from_pretrained('runwayml/stable-diffusion-v1-5')
+pipe.to(TORCH_DEVICE)
 
 # Generate
 for prompt in prompts:
+    print(prompt)
     generator = torch.Generator(TORCH_DEVICE).manual_seed(SEED)
     image = pipe(
         prompt,
         num_inference_steps=STEPS,
-        generator=generator,
-        guidance_scale=1.0
+        generator=generator
     ).images[0]
 
     # Save the image
