364d146d677e1e87b0a244c9d1346bf31f0edee9
Nono Martínez Alonso <mundowarezweb@gmail.com>
2025-05-14 09:40:38 +0200

    Fix load_dataset.py.

--- lora/load_dataset.py
+++ lora/load_dataset.py
@@ -7,7 +7,8 @@ SAMPLES_TO_SHOW = 5
 # Load dataset
 dataset = load_dataset("imagefolder", data_dir=DATASET_DIR)
 
-print(f'\n› The dataset at {DATASET_DIR} contains {len(dataset)} captioned images.\n')
+# Print the number of samples in the dataset.
+print(f'\n› The dataset at {DATASET_DIR} contains {dataset["train"].num_rows} captioned images.\n')
 
 # Print a few samples.
 print(f'› Displaying {SAMPLES_TO_SHOW} samples.\n')
